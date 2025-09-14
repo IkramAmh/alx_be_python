026aa74de2d97aabf8aa4df64cb54ed062e87507
IkramAmh <ikramamhirrach2@gmail.com>
2025-09-14 19:57:04 +0100

    savings track

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,6 @@
+month_income = int(input("Enter your monthly income: "))
+month_expense = int(input("Enter your total monthly expenses: "))
+month_saving = month_income - month_expense
+projected_saving = month_saving * 12 + (month_saving * 12 * 0.05)
+print("Your monthly savings are ", month_saving, "$")
+print("Projected savings after one year, with interest, is: ", projected_saving, "$")
